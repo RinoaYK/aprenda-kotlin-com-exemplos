5b56f7314681930a7ce1af62f073ee67f5527c43
Lidia Yamamura <rinoayk@gmail.com>
2025-11-23 18:24:50 -0300

    Código que faltou

--- 2_controle_de_fluxo/4_verificacoes_igualdade/exemplo1.kt
+++ 2_controle_de_fluxo/4_verificacoes_igualdade/exemplo1.kt
@@ -4,5 +4,11 @@
  * @see [Equality Checks](https://play.kotlinlang.org/byExample/02_control_flow/04_Equality%20Checks)
  */
 fun main() {
+
+    val authors = setOf("Shakespeare", "Hemingway", "Twain")
+	val writers = setOf("Twain", "Shakespeare", "Hemingway")
+
+    println(authors == writers) //estrutura iguais
+	println(authors === writers) //referências iguais
     
  }
--- 2_controle_de_fluxo/5_expressoes_condicionais/exemplo1.kt
+++ 2_controle_de_fluxo/5_expressoes_condicionais/exemplo1.kt
@@ -4,5 +4,19 @@
  * @see [Conditional Expression](https://play.kotlinlang.org/byExample/02_control_flow/05_Conditional%20expression)
  */
 fun main() {
+	
+    fun max(a: Int, b: Int) = if (a > b) a else b
+	
+    println(max(99, -42))
+    
+    fun maxOld(a: Int, b: Int): Int {
+		if(a > b) {
+			return a
+		} else {
+			return b
+		}
+    }
+
+     println(maxOld(99, -42))
     
  }
